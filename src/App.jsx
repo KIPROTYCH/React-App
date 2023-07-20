e49c801130052e68c5f6a0342555b5b023c9b5dd
KIPROTYCH <kiprotych@gmail.com>
2023-07-21 01:45:46 +0300

    Added preventDefault Function

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 
 
 function App () {
-  const [newItem, setNewItem] = useState("Item !")
+  const [newItem, setNewItem] = useState("")
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
 
   return (
   <>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="">New Item</label>
-      <input value = {newItem} type="text" id="item"/>
+      <input value = {newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
 
     </div>
     <button className="btn">Add</button>
